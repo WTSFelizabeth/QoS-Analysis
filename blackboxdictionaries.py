517c64472bb605f700ed79df80eeed8eb22ea082
WTSFelizabeth <elizabeth@weteachscience.org>
2015-04-24 16:03:02 -0700

    Modifications to session v. no session stats and graphs

--- blackboxdictionaries.py
+++ blackboxdictionaries.py
@@ -5,11 +5,11 @@ blackboxlocationlist = ['IBL_Thurs 9:50','IBL_Thurs 10:40','IBL_Thurs 11:30','IB
 'SLHS Thurs 12','SLHS Thurs 1','Testing']
 
 #  Dictionary of blackbox start and end dates
-blackboxstartandendforgraph = {'IBL':[dt.datetime(2015,4,1),dt.datetime(2015,4,22)],'Sierramont':[dt.datetime(2015,4,1),dt.datetime(2015,4,22)],
+blackboxstartandendforgraph = {'IBL':[dt.datetime(2015,3,29),dt.datetime(2015,4,4)],'Sierramont':[dt.datetime(2015,4,1),dt.datetime(2015,4,22)],
 'SLHS':[dt.datetime(2015,4,1),dt.datetime(2015,4,22)],'Testing':[dt.datetime(2015,3,18),dt.datetime(2015,3,25)]}
 
-blackboxstartandendforanalysis = {'IBL':[dt.datetime(2015,3,27),dt.datetime(2015,4,22)],'Sierramont':[dt.datetime(2015,3,19),dt.datetime(2015,4,22)],
-'SLHS':[dt.datetime(2015,3,21),dt.datetime(2015,4,22)],'Testing':[dt.datetime(2015,3,18),dt.datetime(2015,3,25)]}
+blackboxstartandendforanalysis = {'IBL':[dt.datetime(2015,3,27),dt.datetime(2015,4,23)],'Sierramont':[dt.datetime(2015,3,19),dt.datetime(2015,4,23)],
+'SLHS':[dt.datetime(2015,3,21),dt.datetime(2015,4,23)],'Testing':[dt.datetime(2015,3,18),dt.datetime(2015,3,25)]}
 
 #  Convert blackbox file name to location list for that site.
 blackboxlocationconvert = {'pilot-ibl':['IBL_Thurs 9:50','IBL_Thurs 10:40','IBL_Thurs 11:30','IBL_Thurs 2:20'],
@@ -22,18 +22,27 @@ blackboxschoollist = ['IBL','Sierramont','SLHS','Testing']
 blackboxschooltoday = {'IBL':'Thu','Sierramont':'Tue','SLHS':'Thu','Testing':'Mon'}
 
 #  Blackbox days to leave out of analysis (i.e. spring break, etc).
-blackboxdaysout = {'IBL':[dt.datetime(2015,4,6),dt.datetime(2015,4,7),dt.datetime(2015,4,8),dt.datetime(2015,4,9),dt.datetime(2015,4,10),dt.datetime(2015,4,16),
-dt.datetime(2015,4,21)],'Sierramont':[dt.datetime(2015,4,13),dt.datetime(2015,4,14),dt.datetime(2015,4,15),dt.datetime(2015,4,16),dt.datetime(2015,4,21)],
-'SLHS':[dt.datetime(2015,4,16),dt.datetime(2015,4,21)],'Testing':[]}
+blackboxdaysout = {'IBL':[dt.datetime(2015,3,25),dt.datetime(2015,3,26),dt.datetime(2015,4,6),dt.datetime(2015,4,7),dt.datetime(2015,4,8),
+dt.datetime(2015,4,9),dt.datetime(2015,4,10),dt.datetime(2015,4,16),dt.datetime(2015,4,21)],'Sierramont':[dt.datetime(2015,3,16),
+dt.datetime(2015,3,17),dt.datetime(2015,3,18),dt.datetime(2015,4,2),dt.datetime(2015,4,13),
+dt.datetime(2015,4,14),dt.datetime(2015,4,15),dt.datetime(2015,4,16),dt.datetime(2015,4,21)],
+'SLHS':[dt.datetime(2015,3,16),dt.datetime(2015,3,17),dt.datetime(2015,3,19),dt.datetime(2015,4,3),
+dt.datetime(2015,4,13),dt.datetime(2015,4,16),dt.datetime(2015,4,21)],'Testing':[]}
 
 #  Special exceptions
 blackboxspecial = {'IBL':False,'Sierramont':True,'SLHS':False,'Testing':False}
 
 #  Blackbox days to move to non-session days.
-blackboxdaysnosession = {'Sierramont':[dt.datetime(2015,3,24),dt.datetime(2015,3,31),dt.datetime(2015,4,21),dt.datetime(2015,4,28)]}
+blackboxdaysnosession = {'IBL_Thurs 9:50':[],'IBL_Thurs 10:40':[],'IBL_Thurs 11:30':[],'IBL_Thurs 2:20':[],
+'Sierramont Tue 10':[dt.datetime(2015,3,24),dt.datetime(2015,3,31),dt.datetime(2015,4,21),dt.datetime(2015,4,28)],
+'Sierramont Tue 12':[dt.datetime(2015,3,24),dt.datetime(2015,3,31),dt.datetime(2015,4,21),dt.datetime(2015,4,28)],
+'SLHS Thurs 12':[],'SLHS Thurs 1':[],'Testing':[]}
 
 #  Blackbox days that are session days.
-blackboxdayssessions = {'Sierramont':[dt.datetime(2015,3,23),dt.datetime(2015,3,30),dt.datetime(2015,4,20),dt.datetime(2015,4,27)]}
+blackboxdayssessions = {'IBL_Thurs 9:50':[],'IBL_Thurs 10:40':[],'IBL_Thurs 11:30':[],'IBL_Thurs 2:20':[],
+'Sierramont Tue 10':[dt.datetime(2015,3,23),dt.datetime(2015,3,30),dt.datetime(2015,4,20),dt.datetime(2015,4,27)],
+'Sierramont Tue 12':[dt.datetime(2015,3,23),dt.datetime(2015,3,30),dt.datetime(2015,4,20),dt.datetime(2015,4,27)],
+'SLHS Thurs 12':[],'SLHS Thurs 1':[],'Testing':[]}
 
 #  Special start times.
 blackboxspecialtimesstart = {'Sierramont Tue 10':dt.time(13,35,0),'Sierramont Tue 12':dt.time(11,8,0)}
